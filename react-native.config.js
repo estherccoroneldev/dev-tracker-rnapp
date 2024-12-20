a46bb73f4e94c38442ad1cc119539c6dd4f5748f
Esther Coronel <esthercheremo@gmail.com>
2024-12-20 15:39:08 -0300

    chore: adds react navigation packages config and misc

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,12 @@
 module.exports = {
+  dependencies: {
+    ...(process.env.NO_FLIPPER
+      ? {'react-native-flipper': {platforms: {ios: null}}}
+      : {}),
+  },
   project: {
     ios: {
-      automaticPodsInstallation: true
-    }
-  }
-}
+      automaticPodsInstallation: true,
+    },
+  },
+};
