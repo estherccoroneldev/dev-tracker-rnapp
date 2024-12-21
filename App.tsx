7113216ab654ab61b265a323ee002ef0b3224a89
Esther Coronel <esthercheremo@gmail.com>
2024-12-21 16:46:46 -0300

    chore: configs native-base provider

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
+import {NativeBaseProvider, extendTheme} from 'native-base';
 import RootNavigator from './src/navigation/RootNavigator';
+
 import {Provider} from 'react-redux';
 import store from './src/store';
 
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: 'light',
+};
+
+export const theme = extendTheme({config});
+type MyThemeType = typeof theme;
+declare module 'native-base' {
+  interface ICustomTheme extends MyThemeType {}
+}
+
 function App(): React.JSX.Element {
   return (
-    <Provider store={store}>
-      <RootNavigator />
-    </Provider>
+    <NativeBaseProvider>
+      <Provider store={store}>
+        <RootNavigator />
+      </Provider>
+    </NativeBaseProvider>
   );
 }
 
